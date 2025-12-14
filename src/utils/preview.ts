5a1d6ec3668b722026375d768b704f14570753b1
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-12-15 00:20:49 +0900

    feat: Get Feather icon SVG data

--- src/utils/preview.ts
+++ src/utils/preview.ts
@@ -1,3 +1,4 @@
+import * as feather from 'feather-icons';
 import { Branding, BrandingColor, IconColor } from '../types';
 
 /**
@@ -6,10 +7,32 @@ import { Branding, BrandingColor, IconColor } from '../types';
  * @param branding - Object containing color and icon name
  */
 export function createMarkdown(branding: Branding) {
-  const { color } = branding;
+  const { color, icon } = branding;
 
   // Determine icon color based on branding color
   const iconColor =
     color === BrandingColor.White ? IconColor.Black : IconColor.White;
-  console.debug(iconColor);
+  console.debug(`iconColor: ${iconColor}`);
+
+  const iconSvg = getFeatherIconSvg(icon, iconColor);
+  console.debug(`iconSvg: \n${iconSvg}`);
+}
+
+/**
+ * Retrieve a Feather icon SVG string by name.
+ * Throw if the icon name is invalid.
+ *
+ * @param iconName - Name of the Feather icon
+ * @returns SVG string representation of the icon
+ * @throws Error if icon name does not exist in Feather icons
+ */
+function getFeatherIconSvg(iconName: string, iconColor: IconColor): string {
+  const key = iconName as keyof typeof feather.icons;
+  const icon = feather.icons[key];
+
+  if (!icon) {
+    throw new Error(`Unknown Feather icon: "${iconName}"`);
+  }
+
+  return icon.toSvg({ color: iconColor, width: 24, height: 24 });
 }
