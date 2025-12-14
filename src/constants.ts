8f5177cd6ddb7a0b39169b4329af34b699c2608e
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-12-14 18:47:21 +0900

    feat: Trigger hover only on `action.yml` and `action.yaml`

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+// File patterns supported by this extension
+export const TARGET_FILE_PATTERNS = ['**/action.yaml', '**/action.yml'];
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TARGET_FILE_PATTERNS } from './constants';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,6 +11,28 @@ export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "github-actions-branding-preview" is now active!'
   );
+
+  const hoverProvider = vscode.languages.registerHoverProvider(
+    TARGET_FILE_PATTERNS.map((pattern) => ({ pattern })),
+    {
+      provideHover(document, position) {
+        try {
+          console.debug(
+            `Hover triggered in ${document.fileName} at ${position.line}:${position.character}`
+          );
+
+          // TODO: implement hover contents
+          return new vscode.Hover(new vscode.MarkdownString('ok'));
+        } catch (err: unknown) {
+          const message = err instanceof Error ? err.message : String(err);
+          console.error(`Error in provideHover: ${message}`);
+          return;
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(hoverProvider);
 }
 
 // This method is called when your extension is deactivated
