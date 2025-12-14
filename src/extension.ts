16467572de48923ee790bfc9637abdde83da1435
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-12-14 17:50:52 +0900

    fix: Remove templates

--- src/extension.ts
+++ src/extension.ts
@@ -10,22 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "github-actions-branding-preview" is now active!'
   );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'github-actions-branding-preview.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Hello World from GitHub Actions Branding Preview!'
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
